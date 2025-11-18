8d2677612aa852382ce46f90b497762ad51b3330
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-11-17 21:51:44 -0300

    Uso de While

--- CollectionsDemo/Program.cs
+++ CollectionsDemo/Program.cs
@@ -4,18 +4,6 @@
     {
         static void Main(string[] args)
         {
-            // Estruturas de Dados
-
-            // Coleções
-            // COllections
-
-            //Listas
-            // Permite acesso rápido via índice/posição
-            // Quem está na posição 0? -> Lista é eficiente para leituras posicionais
-            // O número 25 está na lista? -> Lista é ineficiente para buscas por um determinado valor
-
-            // Uma coleção genérica supota qualquer tipo de dado
-            // Lista de strings, Lista de Alunos, Lista de Inteiros
             
             var compras = new List<string>();
             
@@ -24,26 +12,23 @@
             compras.Add("Leite");       // Posição 2 (item 3)
             compras.Add("Ovos");        // Posição 3 (item 4)
 
-            // Perguntar par ao usuario se ele deseja adicionar um item na lista de compras?
-            // se sim, perguntar qual o nome do item e adicionar na lista
-            // senão, continua para a exibição da lista completa
-
-
-            // Colocar isso em um laço de repetição
-            string resposta = "";
-            do
+            var sair = false;
+            while (!sair) //O usuário não digitou SAIR
             {
-                Console.Write("Deseja adicionar um item na lista de compras? (s/n): ");
-                resposta = Console.ReadLine();
+                Console.WriteLine("Digite o Nome do Produto ou 'sair' para encerrar:");
+                var entrada = Console.ReadLine();
 
-                if (resposta == "s" || resposta == "S")
+                if (entrada == "sair")
                 {
-                    Console.Write("Digite o item:");
-                    string novoItem = Console.ReadLine();
-                    compras.Add(novoItem);
+                    sair = true;
+                }
+                else
+                {
+                    compras.Add(entrada);
                 }
             }
-            while (resposta == "s" || resposta == "S");
+
+
 
 
             for (int i = 0; i < compras.Count; i++)
